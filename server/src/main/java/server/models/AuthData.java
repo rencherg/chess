543969fb8771757361aee0f62d69f80da4ffffb1
rencherga <rencher.grant@gmail.com>
2024-02-21 16:21:21 -0700

    Created Data Model classes

---
+++ server/src/main/java/server/models/AuthData.java
@@ -0,0 +1,35 @@
+package server.models;
+
+public class AuthData {
+    private String authToken;
+    private String username;
+
+    public AuthData(String authToken, String username) {
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthData{" +
+                "authToken='" + authToken + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
